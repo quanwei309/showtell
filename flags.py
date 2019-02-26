ce7cd2655e0d34ea1049295277c04ac8ea9bb84e
qwgao <120809969@qq.com>
2019-02-26 23:49:11 +0800

    first commit

---
+++ flags.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+import datetime
+
+import pytz
+
+
+tz = pytz.timezone('Asia/Shanghai')
+current_time = datetime.datetime.now(tz)
+
+
+def parse_args(check=True):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--input_train_file_pattern', type=str, default='/data/helloworld309/showandtell-data/train-?????-of-00256',
+                        help='input_train_file_pattern.')
+
+    parser.add_argument('--inception_checkpoint_file', type=str, default='/data/helloworld309/showandtell-data/inception_v3.ckpt',
+                        help='inception_checkpoint_file.')
+
+    parser.add_argument('--train_dir', type=str, default='/data/helloworld309/showandtell-model/train',
+                        help='train_dir.')
+
+    parser.add_argument('--train_inception', type=str, default='False',
+                        help='train_inception.')
+
+    parser.add_argument('--number_of_steps', type=int, default=4000,
+                        help='number_of_steps')
+
+    parser.add_argument('--input_val_file_pattern', type=str, default='/data/helloworld309/showandtell-data/val-?????-of-00004',
+                        help='input_val_file_pattern.')
+
+    parser.add_argument('--checkpoint_dir', type=str, default='/data/helloworld309/showandtell-model/train',
+                        help='checkpoint_dir.')
+
+
+
+    parser.add_argument('--eval_dir', type=str, default='/data/helloworld309/showandtell-model/eval',
+                        help='eval_dir')
+
+    parser.add_argument('--checkpoint_path', type=str, default='/data/helloworld309/showandtell-model/train',
+                        help='checkpoint_path')
+
+    parser.add_argument('--vocab_file', type=str, default='/data/helloworld309/showandtell-data/word_counts.txt',
+                        help='vocab_file')
+    parser.add_argument('--input_files', type=str, default='COCO_val2014_000000224477.jpg',
+                        help='input_files')
+
+    FLAGS, unparsed = parser.parse_known_args()
+
+    return FLAGS, unparsed
+
+
+if __name__ == '__main__':
+    FLAGS, unparsed = parse_args()
+
+    for x in dir(FLAGS):
+        print(getattr(FLAGS, x))
---
+++ train_eval.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+
+
+from flags import parse_args
+
+
+if __name__ == '__main__':
+    FLAGS, unparsed = parse_args()
+    print('current working dir [{0}]'.format(os.getcwd()))
+    w_d = os.path.dirname(os.path.abspath(__file__))
+    print('change wording dir to [{0}]'.format(w_d))
+    os.chdir(w_d)
+
+    cmd = ""
+
+    #train \
+    #--input_train_file_pattern="${MSCOCO_DIR}/train-?????-of-00256" \
+    #--inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
+    #--train_dir="${MODEL_DIR}/train" \
+    #--train_inception=false \
+    #--number_of_steps=1000000
+
+    #/ evaluate \
+    # --input_val_file_pattern = "${MSCOCO_DIR}/val-?????-of-00004" \
+    # --checkpoint_dir = "${MODEL_DIR}/train" \
+    # --eval_dir = "${MODEL_DIR}/eval"
+
+    #run_inference \
+    #--checkpoint_path =${CHECKPOINT_PATH} \
+    #--vocab_file =${VOCAB_FILE} \
+    #--input_files =${IMAGE_FILE}
+    print('################    configuration    ################')
+    p = os.popen('python ./configuration.py')
+    for l in p:
+        print(l.strip())
+
+    for parm in ["input_train_file_pattern", "inception_checkpoint_file", "train_dir", "train_inception", "number_of_steps",
+                 "input_val_file_pattern" , "checkpoint_dir", "eval_dir",
+                 "checkpoint_path", "vocab_file", "input_files"]:
+        try:
+            cmd += ' --{0}={1}'.format(parm, getattr(FLAGS, parm))
+        except:
+            pass
+    #batch_size =32    40000step 大概一个epoch
+    for i in range(10):
+       # train 1 epoch
+        print('################    train    ################')
+        print('################    cmd :   ', cmd)
+        p = os.popen('python ./train.py' + cmd)
+        for l in p:
+            print(l.strip())
+
+        # eval
+        print('################    eval    ################')
+        print('################    cmd :   ', cmd)
+        p = os.popen('python ./evaluate.py' + cmd)
+        for l in p:
+            print(l.strip())
+
+        print('################    inference    ################')
+        print('################    cmd :   ', cmd)
+        p = os.popen('python ./run_inference.py' + cmd)
+        for l in p:
+            print(l.strip())
